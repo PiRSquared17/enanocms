8a07e3a5a5f17f266c984d03260f542fc466cbdd
Dan <Dan@localhost>
2008-07-12 12:01:07 -0400

    Fixed a few more PostgreSQL bugs with insert_id() and an issue with setting SERIAL columns manually. May break things if installed into a database without first dropping tables.

---
+++ install/includes/common.php
@@ -0,0 +1,157 @@
+<?php
+
+/*
+ * Enano - an open-source CMS capable of wiki functions, Drupal-like sidebar blocks, and everything in between
+ * Version 1.1.4 (Caoineag alpha 4)
+ * Copyright (C) 2006-2008 Dan Fuhry
+ * Installation package
+ * common.php - Installer common functions
+ *
+ * This program is Free Software; you can redistribute and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for details.
+ */
+
+// Our version number. This needs to be changed for any custom releases.
+$installer_version = array(
+  'version' => '1.1.4',
+  'type' => 'alpha'
+  // If type is set to "rc", "beta", or "alpha", optionally another version number can be issued with the key 'sub':
+  // 'sub' => '3' will produce Enano 1.1.1a3 / Enano 1.1.1 alpha 3
+);
+
+function installer_enano_version($long = false)
+{
+  global $installer_version;
+  static $keywords = array(
+    'alpha' => 'a',
+    'beta' => 'b',
+    'RC' => 'rc'
+    );
+  $v = $installer_version['version'];
+  if ( isset($installer_version['sub']) )
+  {
+    $v .= ( $short ) ? $keywords[$installer_version['type']] : " {$installer_version['type']} ";
+    $v .= $installer_version['sub'];
+  }
+  return $v;
+}
+ 
+// Determine Enano root directory
+
+if ( !defined('ENANO_ROOT') )
+{
+  $enano_root = dirname(dirname(dirname(__FILE__)));
+  if ( preg_match('#/repo$#', $enano_root) && file_exists("$enano_root/../.enanodev") )
+  {
+    $enano_root = preg_replace('#/repo$#', '', $enano_root);
+  }
+  
+  define('ENANO_ROOT', $enano_root);
+}
+
+chdir(ENANO_ROOT);
+
+// Determine our scriptPath
+if ( isset($_SERVER['REQUEST_URI']) && !defined('scriptPath') )
+{
+  // Use reverse-matching to determine where the REQUEST_URI overlaps the Enano root.
+  $requri = $_SERVER['REQUEST_URI'];
+  if ( isset($_SERVER['PATH_INFO']) && !preg_match('/index\.php$/', $_SERVER['PATH_INFO']) )
+  {
+    $requri = preg_replace(';' . preg_quote($_SERVER['PATH_INFO']) . '$;', '', $requri);
+  }
+  if ( !preg_match('/\.php$/', $requri) )
+  {
+    // user requested http://foo/enano as opposed to http://foo/enano/index.php
+    $requri .= '/index.php';
+  }
+  $sp = dirname($_SERVER['REQUEST_URI']);
+  if ( $sp == '/' || $sp == '\\' )
+  {
+    $sp = '';
+  }
+  $sp = preg_replace('#/install$#', '', $sp);
+  define('scriptPath', $sp);
+}
+
+// is Enano already installed?
+@include(ENANO_ROOT . '/config.php');
+if ( defined('ENANO_INSTALLED') && defined('ENANO_DANGEROUS') )
+{
+  $title = 'Installation locked';
+  require('includes/common.php');
+  $template->header();
+  echo '<p>The installer has detected that an installation of Enano already exists on your server. You MUST delete config.php if you wish to reinstall Enano.</p>';
+  $template->footer();
+  exit();
+}
+
+if ( !function_exists('microtime_float') )
+{
+  function microtime_float()
+  {
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+  }
+}
+
+define('IN_ENANO_INSTALL', 1);
+
+require_once(ENANO_ROOT . '/install/includes/ui.php');
+require_once(ENANO_ROOT . '/includes/functions.php');
+require_once(ENANO_ROOT . '/includes/json.php');
+require_once(ENANO_ROOT . '/includes/constants.php');
+require_once(ENANO_ROOT . '/includes/rijndael.php');
+
+// If we have at least PHP 5, load json2
+if ( version_compare(PHP_VERSION, '5.0.0', '>=') )
+{
+  require_once(ENANO_ROOT . '/includes/json2.php');
+}
+
+strip_magic_quotes_gpc();
+
+// Build a list of available languages
+$dir = @opendir( ENANO_ROOT . '/language' );
+if ( !$dir )
+  die('CRITICAL: could not open language directory');
+
+$languages = array();
+// Use the old PHP4-compatible JSON decoder
+$json = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+
+while ( $dh = @readdir($dir) )
+{
+  if ( $dh == '.' || $dh == '..' )
+    continue;
+  if ( file_exists( ENANO_ROOT . "/language/$dh/meta.json" ) )
+  {
+    // Found a language directory, determine metadata
+    $meta = @file_get_contents( ENANO_ROOT . "/language/$dh/meta.json" );
+    if ( empty($meta) )
+      // Could not read metadata file, continue silently
+      continue;
+    $meta = $json->decode($meta);
+    if ( isset($meta['lang_name_english']) && isset($meta['lang_name_native']) && isset($meta['lang_code']) )
+    {
+      $languages[$meta['lang_code']] = array(
+          'name' => $meta['lang_name_native'],
+          'name_eng' => $meta['lang_name_english'],
+          'dir' => $dh
+        );
+    }
+  }
+}
+
+if ( count($languages) < 1 )
+{
+  die('The Enano installer couldn\'t find any languages in the language/ folder. Enano needs at least one language in this folder to load and install properly.');
+}
+
+// List of available DB drivers
+$supported_drivers = array('mysql', 'postgresql');
+
+?>
---
+++ install/includes/stages/website.php
@@ -0,0 +1,256 @@
+<?php
+
+/*
+ * Enano - an open-source CMS capable of wiki functions, Drupal-like sidebar blocks, and everything in between
+ * Version 1.1.4 (Caoineag alpha 4)
+ * Copyright (C) 2006-2008 Dan Fuhry
+ * Installation package
+ * website.php - Installer website-settings stage
+ *
+ * This program is Free Software; you can redistribute and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for details.
+ */
+
+if ( !defined('IN_ENANO_INSTALL') )
+  die();
+
+// Note: this is called from database_*.php, not directly from install.php
+
+$ui->add_header('<script type="text/javascript" src="includes/js/formutils.js"></script>');
+$ui->show_header();
+
+?>
+
+<script type="text/javascript">
+  // <![CDATA[
+  function ajaxMrwTest()
+  {
+    install_set_ajax_loading();
+    // Send a series of tests to the server, and if we get an "expected" response
+    setTimeout("ajaxGet(scriptPath + '/install/rewrite', __ajaxMrwTest_chain_rewrite);", 750);
+  }
+  var __ajaxMrwTest_chain_rewrite = function()
+  {
+    if ( ajax.readyState == 4 )
+    {
+      if ( ajax.responseText == 'good_rewrite' )
+      {
+        ajaxMrwSet('rewrite');
+      }
+      else
+      {
+        ajaxGet(scriptPath + '/install/install.php/shortened?do=modrewrite_test', __ajaxMrwTest_chain_shortened);
+      }
+    }
+  }
+  var __ajaxMrwTest_chain_shortened = function()
+  {
+    if ( ajax.readyState == 4 )
+    {
+      if ( ajax.responseText == 'good_shortened' )
+      {
+        ajaxMrwSet('shortened');
+      }
+      else
+      {
+        ajaxGet(scriptPath + '/install/install.php?do=modrewrite_test&str=standard', __ajaxMrwTest_chain_standard);
+      }
+    }
+  }
+  var __ajaxMrwTest_chain_standard = function()
+  {
+    if ( ajax.readyState == 4 )
+    {
+      if ( ajax.responseText == 'good_standard' )
+      {
+        ajaxMrwSet('standard');
+      }
+      else
+      {
+        install_unset_ajax_loading();
+        new messagebox(MB_OK | MB_ICONSTOP, $lang.get('website_msg_ajax_test_fail_title'), $lang.get('website_msg_ajax_test_fail_body'));
+      }
+    }
+  }
+  function ajaxMrwSet(level)
+  {
+    install_unset_ajax_loading();
+    if ( !in_array(level, ['rewrite', 'shortened', 'standard']) )
+      return false;
+    
+    document.getElementById('url_radio_rewrite').checked = false;
+    document.getElementById('url_radio_shortened').checked = false;
+    document.getElementById('url_radio_standard').checked = false;
+    document.getElementById('url_radio_' + level).checked = true;
+    document.getElementById('url_radio_' + level).focus();
+    
+    switch ( level )
+    {
+      case 'rewrite':
+        var str = $lang.get('website_msg_bestmethod_rewrite');
+        break;
+      case 'shortened':
+        var str = $lang.get('website_msg_bestmethod_shortened');
+        break;
+      case 'standard':
+        var str = $lang.get('website_msg_bestmethod_standard');
+        break;
+    }
+    document.getElementById('mrw_report').className = 'info-box-mini';
+    document.getElementById('mrw_report').innerHTML = str;
+  }
+  
+  function verify()
+  {
+    var frm = document.forms['install_website'];
+    var fail = false;
+    if ( frm.site_name.value == '' )
+    {
+      fail = true;
+      new Spry.Effect.Shake($(frm.site_name).object, {duration: 750}).start();
+      frm.site_name.focus();
+    }
+    if ( frm.site_desc.value == '' )
+    {
+      new Spry.Effect.Shake($(frm.site_desc).object, {duration: 750}).start();
+      if ( !fail )
+        frm.site_desc.focus();
+      fail = true;
+    }
+    if ( frm.copyright.value == '' )
+    {
+      new Spry.Effect.Shake($(frm.copyright).object, {duration: 750}).start();
+      if ( !fail )
+        frm.copyright.focus();
+      fail = true;
+    }
+    return ( !fail );
+  }
+  // ]]>
+</script>
+
+<form action="install.php?stage=login" method="post" name="install_website" onsubmit="return verify();"><?php
+  foreach ( $_POST as $key => &$value )
+  {
+    if ( !preg_match('/^[a-z0-9_]+$/', $key) )
+      die('You idiot hacker...');
+    if ( $key == '_cont' )
+      continue;
+    $value_clean = str_replace(array('\\', '"', '<', '>'), array('\\\\', '\\"', '&lt;', '&gt;'), $value);
+    echo "\n  <input type=\"hidden\" name=\"$key\" value=\"$value_clean\" />";
+  }
+  
+  $https = ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' );
+  $scriptpath_full = 'http' . ( $https ? 's' : '' ) . '://' . $_SERVER['HTTP_HOST'] . scriptPath . '/';
+  ?>
+  
+  <table border="0" cellspacing="0" cellpadding="10">
+  
+    <tr>
+      <td>
+        <b><?php echo $lang->get('website_field_name'); ?></b><br />
+        <span id="hint_site_name" class="fieldtip"><?php echo $lang->get('website_field_name_hint'); ?></span>
+      </td>
+      <td style="width: 50%;">
+        <input type="text" name="site_name" size="50" tabindex="1" />
+      </td>
+    </tr>
+    
+    <tr>
+      <td>
+        <b><?php echo $lang->get('website_field_desc'); ?></b><br />
+        <span id="hint_site_desc" class="fieldtip"><?php echo $lang->get('website_field_desc_hint'); ?></span>
+      </td>
+      <td>
+        <input type="text" name="site_desc" size="50" tabindex="2" />
+      </td>
+    </tr>
+    
+    <tr>
+      <td>
+        <b><?php echo $lang->get('website_field_copyright'); ?></b><br />
+        <span id="hint_copyright" class="fieldtip"><?php echo $lang->get('website_field_copyright_hint'); ?></span>
+      </td>
+      <td>
+        <input type="text" name="copyright" size="50" tabindex="3" />
+      </td>
+    </tr>
+    
+    <tr>
+      <td valign="top">
+        <b><?php echo $lang->get('website_field_urlscheme'); ?></b><br />
+        <?php echo $lang->get('website_field_urlscheme_hint'); ?>
+      </td>
+      <td>
+      
+        <table border="0" cellpadding="10" cellspacing="0">
+          <tr>
+            <td valign="top">
+              <input type="radio" name="url_scheme" value="standard" id="url_radio_standard" tabindex="5" />
+            </td>
+            <td>
+              <label for="url_radio_standard">
+                <b><?php echo $lang->get('website_field_urlscheme_opt_standard'); ?></b>
+              </label>
+              <span class="fieldtip" id="hint_url_scheme_standard">
+                <p><?php echo $lang->get('website_field_urlscheme_opt_standard_hint'); ?></p>
+                <p><small><b><?php echo $lang->get('website_field_urlscheme_lbl_example'); ?></b> <tt><?php echo $scriptpath_full . 'index.php?title=Page'; ?></tt></small></p>
+              </span>
+            </td>
+          </tr>
+        </table>
+        
+        <table border="0" cellpadding="10" cellspacing="0">
+          <tr>
+            <td valign="top">
+              <input type="radio" checked="checked" name="url_scheme" value="shortened" id="url_radio_shortened" tabindex="5" />
+            </td>
+            <td>
+              <label for="url_radio_shortened">
+                <b><?php echo $lang->get('website_field_urlscheme_opt_shortened'); ?></b>
+              </label>
+              <span class="fieldtip" id="hint_url_scheme_shortened">
+                <p><?php echo $lang->get('website_field_urlscheme_opt_shortened_hint'); ?></p>
+                <p><small><b><?php echo $lang->get('website_field_urlscheme_lbl_example'); ?></b> <tt><?php echo $scriptpath_full . 'index.php/Page'; ?></tt></small></p>
+              </span>
+            </td>
+          </tr>
+        </table>
+        
+        <table border="0" cellpadding="10" cellspacing="0">
+          <tr>
+            <td valign="top">
+              <input type="radio" name="url_scheme" value="rewrite" id="url_radio_rewrite" tabindex="5" />
+            </td>
+            <td>
+              <label for="url_radio_rewrite">
+                <b><?php echo $lang->get('website_field_urlscheme_opt_rewrite'); ?></b>
+              </label>
+              <span id="hint_url_scheme_rewrite" class="fieldtip">
+                <p><?php echo $lang->get('website_field_urlscheme_opt_rewrite_hint'); ?></p>
+                <p><small><b><?php echo $lang->get('website_field_urlscheme_lbl_example'); ?></b> <tt><?php echo $scriptpath_full . 'Page'; ?></tt></small></p>
+              </span>
+            </td>
+          </tr>
+        </table>
+        
+        <p>
+          <a href="#mrw_scan" onclick="ajaxMrwTest(); return false;" tabindex="4"><?php echo $lang->get('website_btn_urlscheme_detect'); ?></a>
+        </p>
+        
+        <div id="mrw_report"></div>
+        
+      </td>
+    </tr>
+    
+  </table>
+  
+  <div style="text-align: center;">
+    <input type="submit" name="_cont" value="<?php echo $lang->get('meta_btn_continue'); ?>" tabindex="6" />
+  </div>
+  
+</form>
+
